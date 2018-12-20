76b37644a5ab63529175a438b6139781300eb222
dalepotter <dalepotter@gmail.com>
2018-12-20 06:44:32 +0000

    Revert "Deploy dalepotter/archive-o-tron to github.com/dalepotter/archive-o-tron.git:master"
    This reverts commit 9b8ab7818c31b66ba0edbc94449d65a86fb6917f as it was a failed attempt to use TravisCI deploy stage to commit keeping old files on the `master` branch.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-This folder is where the output HTML files will be stored.
+# archive-o-tron
+
+Simple bash script to download a URL and save the output.
+
+## Setup
+
+[Coming soon!]
---
+++ archiver.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+declare -A urls=(
+  ["IATI_Dashboard_dev_Humanitarian"]="http://dev.dashboard.iatistandard.org/humanitarian.html"
+  ["IATI_Dashboard_Humanitarian"]="http://dashboard.iatistandard.org/humanitarian.html"
+  ["Grand_Bargain_Dashboard"]="http://46.101.46.6/dashboard"
+)
+
+#Then use them just like normal arrays. animals['key']='value' to set value, "${animals[@]}" expands the values, "${!animals[@]}" (notice the !) expands the keys. Don't forget to quote them:
+echo "Found ${#urls[*]} pages to download..."
+
+echo "Starting loop:"
+for slug in "${!urls[@]}"; do
+  echo "Processing $slug"
+  output_dir=docs/
+  seperator=_
+  file_extension=.html
+  output_filename=$output_dir$(date -u +"%Y-%m-%dT%H:%M:%SZ")$seperator$slug$file_extension
+  echo "Downloading ${urls[$slug]}"
+  echo "Saving as $output_filename"
+  wget ${urls[$slug]} -O $output_filename
+  echo "Finished $slug"; done
+
+echo 'Completed loop'
