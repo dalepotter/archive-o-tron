fe069bb91f73c965b76be312ccf9a41673c0632c
dalepotter <dalepotter@gmail.com>
2018-12-20 10:09:55 +0000

    Add script to fetch existing files and add them to the output

---
+++ add_existing_files.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -x # Turn echo on
+
+PUBLICATION_BRANCH=gh-pages
+git clone --branch=$PUBLICATION_BRANCH https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_REPO_SLUG-$PUBLICATION_BRANCH 2>&1 > /dev/null
+cp -r $TRAVIS_REPO_SLUG-$PUBLICATION_BRANCH docs/
